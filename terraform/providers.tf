d320cfaa11ca8f136bb81af46ea3db8b487ac28f
Rhythm Jain <rhythmjain523@gmail.com>
2026-05-05 06:45:51 +0530

    final

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "devops-tf-bucket-2026"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
